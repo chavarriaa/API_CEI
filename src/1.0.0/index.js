3c2991a05180aa50e7a68c39d968934967f2af10
Diego Montoya <diegolink4@hotmail.com>
2022-08-09 10:43:49 -0600

    change npm start commit

--- src/1.0.0/index.js
+++ src/1.0.0/index.js
@@ -10,12 +10,12 @@ app.set('json spaces',2);
 app.use(express.json());
 app.use(cors())
 
-app.use('/1.0.0',require('./src/1.0.0/routes/Post'))
-app.use('/1.0.0',require('./src/1.0.0/routes/Usuario'))
+app.use('/1.0.0',require('./routes/Post'))
+app.use('/1.0.0',require('./routes/Usuario'))
 
-app.use('/1.0.0',require('./src/1.0.0/routes/Postulacion'))
-app.use('/1.0.0',require('./src/1.0.0/routes/imagenes'))
-app.use('/1.0.0',require('./src/1.0.0/routes/Contactanos'))
+app.use('/1.0.0',require('./routes/Postulacion'))
+app.use('/1.0.0',require('./routes/imagenes'))
+app.use('/1.0.0',require('./routes/Contactanos'))
 
 const server = app.listen(app.get('port'),(req,res)=>{
     console.log(`Server started at http://localhost:${app.get('port')}`)
